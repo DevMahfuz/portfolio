bcf0e4ff9a7075e255b6e11d6cd79e1e5ffd0a2f
Mahfuz <mahfuzm444@gmail.com>
2024-07-05 23:19:47 +0600

    update image component

--- components/testimonial/reviewSlider.jsx
+++ components/testimonial/reviewSlider.jsx
@@ -3,6 +3,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 const ReviewSlider = () => {
   var settings = {
     dots: true,
@@ -59,7 +60,12 @@ const ReviewSlider = () => {
                   <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                 </svg>
               </div>
-              <img src="/img/testimonials/1.webp" alt="" />
+              <Image
+                src="/img/testimonials/1.webp"
+                alt=""
+                width={100}
+                height={100}
+              />
 
               <h3 className="client-name">itsyourdestiny</h3>
               <p className="client-country">United States </p>
@@ -118,7 +124,12 @@ const ReviewSlider = () => {
                   <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                 </svg>
               </div>
-              <img src="/img/testimonials/2.webp" alt="" />
+              <Image
+                src="/img/testimonials/2.webp"
+                alt=""
+                width={100}
+                height={100}
+              />
 
               <h3 className="client-name">blazingospel</h3>
               <p className="client-country">United Kingdom</p>
@@ -174,7 +185,12 @@ const ReviewSlider = () => {
                   <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                 </svg>
               </div>
-              <img src="/img/testimonials/3.webp" alt="" />
+              <Image
+                src="/img/testimonials/3.webp"
+                alt=""
+                width={100}
+                height={100}
+              />
 
               <h3 className="client-name">cschanner</h3>
               <p className="client-country">Sweden</p>
@@ -227,7 +243,12 @@ const ReviewSlider = () => {
                   <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                 </svg>
               </div>
-              <img src="/img/testimonials/2.webp" alt="" />
+              <Image
+                src="/img/testimonials/4.webp"
+                alt=""
+                width={100}
+                height={100}
+              />
 
               <h3 className="client-name">hdoptimus</h3>
               <p className="client-country">United States</p>
@@ -280,7 +301,12 @@ const ReviewSlider = () => {
                   <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                 </svg>
               </div>
-              <img src="/img/testimonials/5.webp" alt="" />
+              <Image
+                src="/img/testimonials/5.webp"
+                alt=""
+                width={100}
+                height={100}
+              />
               <h3 className="client-name">misswissame</h3>
               <p className="client-country">Morocco</p>
             </div>
@@ -332,7 +358,12 @@ const ReviewSlider = () => {
                   <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                 </svg>
               </div>
-              <img src="/img/testimonials/6.webp" alt="" />
+              <Image
+                src="/img/testimonials/6.webp"
+                alt=""
+                width={100}
+                height={100}
+              />
 
               <h3 className="client-name">oliviakenned904</h3>
               <p className="client-country">Australia</p>
@@ -391,7 +422,12 @@ const ReviewSlider = () => {
                   <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                 </svg>
               </div>
-              <img src="/img/testimonials/7.webp" alt="" />
+              <Image
+                src="/img/testimonials/7.webp"
+                alt=""
+                width={100}
+                height={100}
+              />
 
               <h3 className="client-name">dylancooper905</h3>
               <p className="client-country">United Kingdom</p>

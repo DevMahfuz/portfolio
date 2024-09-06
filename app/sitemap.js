34be3295ebdb95b3859784438d2746c5b9f745fc
Mahfuz <mahfuzm444@gmail.com>
2024-09-06 19:23:39 +0600

    add site map

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://mahfuzur.me/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}

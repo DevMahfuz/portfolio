89810bc83511524293067533cad0692ecc89c165
Mahfuz <mahfuzm444@gmail.com>
2025-12-20 22:49:48 +0600

    Fix contact form

--- components/contact/contact.jsx
+++ components/contact/contact.jsx
@@ -23,18 +23,12 @@ const Contact = () => {
   };
   return (
     <section id="contact" className="contact">
-      
       <div className="container contact-wrapper">
-        
         <div className="left">
-          
           <h3>Contact Me</h3>
           <div className="contact-option">
-            
             <div className="contact-option-icon">
-              
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                
                 <path
                   fill="currentColor"
                   d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z"
@@ -42,16 +36,12 @@ const Contact = () => {
               </svg>
             </div>
             <div className="contact-option-text">
-              
               <h4>Call Me</h4> <a href="tel:+8801746885259">+880 1746 885259</a>
             </div>
           </div>
           <div className="contact-option">
-            
             <div className="contact-option-icon">
-              
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                
                 <path
                   fill="currentColor"
                   d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"
@@ -59,17 +49,13 @@ const Contact = () => {
               </svg>
             </div>
             <div className="contact-option-text">
-              
               <h4>Email me</h4>
               <a href="mailto:mahfuzm444@gmail.com">mahfuzm444@gmail.com</a>
             </div>
           </div>
           <div className="contact-option">
-            
             <div className="contact-option-icon">
-              
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                
                 <path
                   fill="currentColor"
                   d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"
@@ -77,19 +63,15 @@ const Contact = () => {
               </svg>
             </div>
             <div className="contact-option-text">
-              
               <h4>My Address</h4>
               <p>House NO: 227, Ward: 6, Bhangura, Pabna, Bangladesh</p>
             </div>
           </div>
         </div>
         <div className="right">
-          
           {!isFormSubmited ? (
             <form onSubmit={onSubmit}>
-              
               <div className="name-wraper">
-                
                 <input
                   type="text"
                   name="first-name"
@@ -104,25 +86,17 @@ const Contact = () => {
                 />
               </div>
               <div className="mail-wrapper">
-                
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  required
-                />
+                <input type="email" name="email" placeholder="Email" required />
               </div>
               <div className="subject-wrapper">
-                
                 <input
                   type="text"
-                  name="subject"
-                  placeholder="subject"
+                  name="user_subject"
+                  placeholder="Subject"
                   required
                 />
               </div>
               <div className="text-area-wrapper">
-                
                 <textarea
                   name="message"
                   placeholder="Type your message here"
@@ -130,13 +104,11 @@ const Contact = () => {
                 />
               </div>
               <button className="submit-btn" type="submit">
-                
                 Send Message
               </button>
             </form>
           ) : (
             <div className="form-success-massate">
-              
               <svg
                 width="100"
                 height="100"
@@ -144,7 +116,6 @@ const Contact = () => {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                
                 <path
                   d="M26.6666 50L46.6666 66.6667L73.3333 33.3333M50 96.6667C43.8716 96.6667 37.8033 95.4596 32.1414 93.1144C26.4796 90.7692 21.3351 87.3317 17.0017 82.9983C12.6683 78.6649 9.23082 73.5204 6.8856 67.8586C4.54038 62.1967 3.33331 56.1283 3.33331 50C3.33331 43.8716 4.54038 37.8033 6.8856 32.1414C9.23082 26.4796 12.6683 21.3351 17.0017 17.0017C21.3351 12.6683 26.4796 9.23084 32.1414 6.88562C37.8033 4.5404 43.8716 3.33333 50 3.33333C62.3767 3.33333 74.2466 8.24998 82.9983 17.0017C91.75 25.7534 96.6666 37.6232 96.6666 50C96.6666 62.3768 91.75 74.2466 82.9983 82.9983C74.2466 91.75 62.3767 96.6667 50 96.6667Z"
                   stroke="currentColor"
@@ -152,11 +123,9 @@ const Contact = () => {
                 ></path>
               </svg>
               <h4 style={{ color: "green", fontSize: "2rem" }}>
-                
                 Form submitted successfully!
               </h4>
               <p>
-                
                 Thank you! The form has been submitted successfully. I will
                 reply to you soon!
               </p>
